98420d342853fda9bbe166187f4a731e8a3acc16
shreyaUx <shreya.patil.11097@gmail.com>
2024-10-18 15:01:56 +0530

    added extra files

---
+++ src/InfoTest/InfoTest.java
@@ -0,0 +1,10 @@
+package InfoTest;
+
+public class InfoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("infotest");
+	}
+
+}
---
+++ src/javaCode/InfoClass.java
@@ -0,0 +1,11 @@
+package javaCode;
+
+public class InfoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my info");
+		System.out.println("new info");
+	}
+
+}
